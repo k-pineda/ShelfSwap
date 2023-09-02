5bb65657fad450e2e7c9a3cc4bfd1ab616b1adfd
spereira15 <105558705+spereira15@users.noreply.github.com>
2023-09-02 17:50:14 -0400

    update resolvers; remove stripe

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,6 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book, Category, Order } = require('../models');
+const { User, Book, Category } = require('../models');
 const { signToken } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
   Query: {
@@ -28,63 +27,12 @@ const resolvers = {
     },
     user: async (parent, args, context) => {
       if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.books',
-          populate: 'category'
-        });
-
-        user.orders.sort((a, b) => b.purchaseDate - a.purchaseDate);
+        const user = await User.findById(context.user._id);
 
         return user;
       }
 
       throw new AuthenticationError('Not logged in');
-    },
-    order: async (parent, { _id }, context) => {
-      if (context.user) {
-        const user = await User.findById(context.user._id).populate({
-          path: 'orders.books',
-          populate: 'category'
-        });
-
-        return user.orders.id(_id);
-      }
-
-      throw new AuthenticationError('Not logged in');
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      await new Order({ books: args.books });
-      // eslint-disable-next-line camelcase
-      const line_items = [];
-
-      // eslint-disable-next-line no-restricted-syntax
-      
-      // *NOT SURE IF WE NEED THIS*
-      // for (const book of args.books) {
-      //   line_items.push({
-      //     price_data: {
-      //       currency: 'usd',
-      //       book_data: {
-      //         title: book.title,
-      //         description: book.description,
-      //         images: [`${url}/images/${book.image}`]
-      //       },
-      //       unit_amount: book.price * 100,
-      //     },
-      //     quantity: book.purchaseQuantity,
-      //   });
-      // }
-
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`,
-      });
-
-      return { session: session.id };
     },
   },
   Mutation: {
@@ -94,17 +42,9 @@ const resolvers = {
 
       return { token, user };
     },
-    addOrder: async (parent, { books }, context) => {
-      console.log(context);
-      if (context.user) {
-        const order = new Order({ books });
-
-        await User.findByIdAndUpdate(context.user._id, { $push: { orders: order } });
-
-        return order;
-      }
-
-      throw new AuthenticationError('Not logged in');
+    addBook: async (parent, args) => {
+      const book = await Book.create(args);
+      return book;
     },
     updateUser: async (parent, args, context) => {
       if (context.user) {
