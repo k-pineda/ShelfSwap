03846c33bb24e9cd3b1c65e2b49d3ced5029c807
Anthony-D99 <anthonydisaf@yahoo.com>
2023-09-11 18:41:13 -0400

    attempting to pull

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -25,15 +25,15 @@ const SearchBooks = () => {
   const [searchInput, setSearchInput] = useState('');
 
   // create state to hold saved bookId values
-  // const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // const [saveBook, { error }] = useMutation(SAVE_BOOK);
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
-  // useEffect(() => {
-  //   return () => saveBookIds(savedBookIds);
-  // });
+  useEffect(() => {
+    return () => saveBookIds(savedBookIds);
+  });
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -68,30 +68,30 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
-  // const handleSaveBook = async (bookId) => {
-  //   // find the book in `searchedBooks` state by the matching id
+  const handleSaveBook = async (bookId) => {
+    // find the book in `searchedBooks` state by the matching id
 
-  //   const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
 
-  //   // get token
-  //   const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // get token
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  //   if (!token) {
-  //     return false;
-  //   }
+    if (!token) {
+      return false;
+    }
 
-  //   try {
-  //     const { data } = await saveBook({
-  //       variables: {bookData: {...bookToSave} }
-  //     })
+    try {
+      const { data } = await saveBook({
+        variables: {bookData: {...bookToSave} }
+      })
 
-  //     // if book successfully saves to user's account, save book id to state
-  //     setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+      // if book successfully saves to user's account, save book id to state
+      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <>
@@ -143,7 +143,7 @@ const SearchBooks = () => {
                     <Card.Title>{book.title}</Card.Title>
                     <p className='small'>Authors: {book.authors}</p>
                     <Card.Text>{book.description}</Card.Text>
-                    {/* {Auth.loggedIn() && (
+                    {Auth.loggedIn() && (
                       <Button
                         disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
                         className='btn-block btn-info'
@@ -152,7 +152,7 @@ const SearchBooks = () => {
                           ? 'This book has already been saved!'
                           : 'Save this Book!'}
                       </Button>
-                    )} */}
+                    )}
                   </Card.Body>
                 </Card>
               </Col>
