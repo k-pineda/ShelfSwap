1fbbab9db0f3fbbc86ec70ce9683800da9a9d39d
Joe Rivera <dvs_joe@hotmail.com>
2023-09-15 14:44:51 -0400

    fixed signup

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useQuery, useMutation } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import { GET_CHAT_BY_ID, GET_CHAT_MESSAGES, GET_USER_CHATS } from "../utils/queries";
+import { GET_CHAT_BY_ID, GET_CHAT_MESSAGES } from "../utils/queries";
 import { SEND_MESSAGE } from "../utils/mutations";
-import ChatList from '../components/ChatList/ChatList';
 import AuthService from "../utils/auth";
 import jwt_decode from "jwt-decode";
 
@@ -16,9 +15,6 @@ const Chat = () => {
   const [messageText, setMessageText] = useState("");
   const { loading: chatLoading, data: chatData } = useQuery(GET_CHAT_BY_ID, {
     variables: { chatId: chat_id },
-  });
-  const { loading: chatsLoading, data: chatsData } = useQuery(GET_USER_CHATS, {
-    variables: { userId },
   });
   const { loading: messagesLoading, data: messagesData, refetch } = useQuery(
     GET_CHAT_MESSAGES,
@@ -33,8 +29,7 @@ const Chat = () => {
       const chatContainer = document.getElementById("chat-container");
       chatContainer.scrollTop = chatContainer.scrollHeight;
     }
-    console.log(chatsData)
-  }, [messagesData, chatsData]);
+  }, [messagesData]);
 
   const handleSendMessage = async () => {
     if (messageText.trim() === "") return;
@@ -54,7 +49,7 @@ const Chat = () => {
     }
   };
 
-  if (chatLoading || messagesLoading || chatsLoading) {
+  if (chatLoading || messagesLoading) {
     return <p>Loading...</p>;
   }
 
@@ -63,54 +58,37 @@ const Chat = () => {
     chatMessages = messagesData.chatMessages;
   }
 
-  const isChatSelected = Boolean(chat_id);
-
+  console.log(chatMessages)
   return (
     <div className="chat-container" id="chat-container">
-      {/* Left side with ChatList */}
-      <div className="chat-list-container">
-        <ChatList chats={chatsData?.userChats} userId={userId} />
-      </div>
       <div className="chat-messages">
-        {isChatSelected ? (
-          // If chat_id is provided, render chat messages
-          chatMessages.map((message) => (
-            <div
-              key={message._id}
-              className={`message ${
-                message.sender === userId ? "own-message" : "other-message"
-              }`}
-            >
-              <p>
-                <span className="username">
-                  {message.sender === userId ? "You" : message.sender.username}
-                </span>
-                {message.text}
-              </p>
-            </div>
-          ))
-        ) : (
-          // If chat_id is not provided, render a "Select chat to start messaging" message
-          <div className="select-chat-message">
-            <p>Select a chat to start messaging</p>
+        {chatMessages.map((message) => (
+          <div
+            key={message._id}
+            className={`message ${
+              message.sender === userId ? "own-message" : "other-message"
+            }`}
+          >
+            <p>
+            <span className="username">
+              {message.sender === userId ? "You" : message.sender.username}
+            </span>
+            {message.text}
+          </p>
           </div>
-        )}
+        ))}
       </div>
-      {isChatSelected && (
-        // Render input field and send button only when chat_id is provided
-        <div className="message-input">
-          <input
-            type="text"
-            placeholder="Type your message..."
-            value={messageText}
-            onChange={(e) => setMessageText(e.target.value)}
-          />
-          <button onClick={handleSendMessage}>Send</button>
-        </div>
-      )}
+      <div className="message-input">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={messageText}
+          onChange={(e) => setMessageText(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
     </div>
   );
 };
-
 
 export default Chat;
