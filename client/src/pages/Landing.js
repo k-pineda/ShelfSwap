c126817d572a2b74d3417902dd3b79026b87c56b
k-pineda <karenp9310@gmail.com>
2023-09-24 21:34:40 -0400

    updated landing js

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,148 +1,23 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import carouselfPic1 from "../assets/carouself1.jpg";
-import carouselfPic2 from "../assets/carouself2.jpg";
-import carouselfPic3 from "../assets/carouself3.jpg";
-import instructionsImage from "../assets/instructions.JPG";
-import whatIsShelfImage from "../assets/whatisshelf.JPG";
+import { useNavigate } from "react-router-dom";
+import Auth from "../utils/auth"
+import Container from "react-bootstrap/Container";
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-const images = [
-  {
-    imgPath: carouselfPic1,
-  },
-  {
-    imgPath: carouselfPic2,
-  },
-  {
-    imgPath: carouselfPic3,
-  },
-];
-
-function SwipeableTextMobileStepper() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = images.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
+export default function Landing() {
+  const navigate = useNavigate();
+  const loggedIn = Auth.loggedIn()
 
   return (
-    <Box className='col-12' sx={{ flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 50,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{images[activeStep].label}</Typography>
-      </Paper>
-      <AutoPlaySwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div key={step.label}>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: 255,
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%',
-                }}
-                src={step.imgPath}
-                alt={step.label}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-      
-      {/* "How It Works" Section */}
-      <Box mt={4} display="flex" flexDirection="column" alignItems="center">
-        {/* Image Slots */}
-        <Box mt={2} position="">
-          <img
-            src={whatIsShelfImage}
-            alt="Image 2"
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              width: "30%",
-              height: "auto",
-              zIndex: 1,
-            }}
-          />
-          <img
-            src={instructionsImage}
-            alt="Image 1"
-            style={{ width: "100%", height: "auto", marginTop:'300px' }}
-          />
-          {/* Add a third image here using the same pattern */}
-        </Box>
-      </Box>
-    </Box>
+    <div className="homepage">
+      {<Container className="home d-flex flex-column align-items-center justify-content-center flex-wrap text-center">
+        <h1 className="home-title">Your Digital Bookshelf: Books at Your Fingertips</h1>
+        <p className="home-text">
+        List, Swap, and Discover New Books!
+        </p>
+        {loggedIn ?
+          (<button className="home-btn" onClick={() => navigate("/exercise")}>Add Exercise</button>) :
+          (<button className="home-btn" onClick={() => navigate("/signup")}>Get Started</button>)}
+      </Container> }
+    </div>
   );
 }
-
-export default SwipeableTextMobileStepper;
